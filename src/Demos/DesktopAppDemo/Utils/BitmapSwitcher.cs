29d2b85bad0d5285ee1417c1b5e435a87d1df921
withsalt <sysrun@163.com>
2025-10-28 19:09:12 +0800

    fix: update

---
+++ src/Demos/DesktopAppDemo/Utils/BitmapSwitcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+
+namespace DesktopAppDemo.Utils
+{
+    sealed class BitmapSwitcher : IDisposable
+    {
+        private readonly WriteableBitmap?[] _tempImage = new WriteableBitmap?[2];
+        private int _currentImageIndex = 0;
+        private readonly object _sync = new();
+
+        public WriteableBitmap? CurrentBitmap
+        {
+            get
+            {
+                lock (_sync)
+                {
+                    _currentImageIndex = _currentImageIndex == 1 ? 0 : 1;
+                    return _tempImage[_currentImageIndex];
+                }
+            }
+            set
+            {
+                lock (_sync)
+                {
+                    _tempImage[_currentImageIndex] = value;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (_sync)
+            {
+                for (int i = 0; i < _tempImage.Length; i++)
+                {
+                    if (_tempImage[i] != null)
+                    {
+                        _tempImage[i]?.Dispose();
+                        _tempImage[i] = null;
+                    }
+                }
+                _currentImageIndex = 0;
+            }
+        }
+    }
+}
--- src/Demos/DesktopAppDemo/ViewModels/MainWindowViewModel.cs
+++ src/Demos/DesktopAppDemo/ViewModels/MainWindowViewModel.cs
@@ -28,48 +28,6 @@ using WithSalt.FFmpeg.Recorder.Models;
 
 namespace DesktopAppDemo.ViewModels
 {
-    sealed class BitmapSwitcher : IDisposable
-    {
-        private readonly WriteableBitmap?[] _tempImage = new WriteableBitmap?[2];
-        private int _currentImageIndex = 0;
-        private readonly object _sync = new();
-
-        public WriteableBitmap? CurrentBitmap
-        {
-            get
-            {
-                lock (_sync)
-                {
-                    _currentImageIndex = _currentImageIndex == 1 ? 0 : 1;
-                    return _tempImage[_currentImageIndex];
-                }
-            }
-            set
-            {
-                lock (_sync)
-                {
-                    _tempImage[_currentImageIndex] = value;
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            lock (_sync)
-            {
-                for (int i = 0; i < _tempImage.Length; i++)
-                {
-                    if (_tempImage[i] != null)
-                    {
-                        _tempImage[i]?.Dispose();
-                        _tempImage[i] = null;
-                    }
-                }
-                _currentImageIndex = 0;
-            }
-        }
-    }
-
     internal partial class MainWindowViewModel : ViewModelBase
     {
         private readonly ILogger<MainWindowViewModel> _logger;
